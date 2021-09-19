ab6c9d287e84dc2881dcfaaaeb532b62f484cf6c
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-09-19 14:46:20 +0200

    PhoneMarketNavigation ui

---
+++ src/components/PhoneMarketPhoneBrandsListItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhoneMarketPhoneBrandsListItem: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PhoneMarketPhoneBrandsListItem;
